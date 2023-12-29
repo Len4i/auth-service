7914d68b5f8121ceb95bf0a9fdb53919dc33b76f
Leonid Podolinskiy <leonidp@lightrun.com>
2023-12-29 20:24:01 +0200

    typo

--- api/v1/versionEntiny.go
+++
@@ -1,9 +0,0 @@
-package v1
-
-type VersionEntity struct {
-	Version       string            `json:"version"`
-	LatestVersion string            `json:"latest_version,omitempty"`
-	Name          string            `json:"name"`
-	Type          string            `json:"type"`
-	Tags          map[string]string `json:"tags"`
-}
